6ec648ace71465b0ab0dc8f90e058e74bd344a3e
Ivan Ramos <ivanandesramos.io@gmail.com>
2024-03-16 08:09:43 +0800

    add search query

---
+++ src/pages/inventory/AddDevice.jsx
@@ -0,0 +1,225 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Input,
+  Spinner,
+  Textarea,
+  Typography,
+} from "@material-tailwind/react";
+import Sidebar from "../../components/Sidebar";
+import { MdAdd, MdArrowBack } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { URL } from "../../config/global";
+
+export default function AddDevice() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+  const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+
+        const response = await axios.get(`${URL}/categories`);
+
+        console.log(response.data);
+        setCategories(response.data);
+
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  async function onSubmit(data) {
+    try {
+      setIsLoading(true);
+
+      data.categoryId = parseInt(data.categoryId);
+
+      const response = await axios.post(`${URL}/devices`, data);
+
+      console.log(response);
+
+      setIsLoading(false);
+      navigate("/inventory");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <main className="bg-[#fdfdfd] h-screen">
+      <Sidebar />
+
+      <section className="ml-[17rem] p-8">
+        {isLoading ? (
+          <div className="w-full h-screen flex justify-center items-center">
+            <Spinner />
+          </div>
+        ) : (
+          <div>
+            <Card className="h-full w-full shadow-none bg-[#fdfdfd]">
+              <CardHeader
+                floated={false}
+                shadow={false}
+                className="rounded-none"
+              >
+                <div className="mb-8 flex items-center justify-between gap-8">
+                  <div className="flex gap-3">
+                    <MdArrowBack
+                      className="w-6 h-6 mt-1 hover:cursor-pointer"
+                      onClick={() => navigate("/inventory")}
+                    />
+                    <div>
+                      <Typography variant="h5" color="blue-gray">
+                        Add New Device
+                      </Typography>
+
+                      <Typography color="gray" className="mt-1 font-normal">
+                        Please fill up the form to add your new device
+                      </Typography>
+                    </div>
+                  </div>
+                </div>
+              </CardHeader>
+              <CardBody className="">
+                <form className="w-5/6" onSubmit={handleSubmit(onSubmit)}>
+                  <div className="w-2/5 mb-6 flex flex-col gap-5">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="-mb-3 font-bold"
+                    >
+                      Device Name
+                      {errors.name && (
+                        <span className="ml-3 text-xs text-red-400 italic">
+                          *This field is required
+                        </span>
+                      )}
+                    </Typography>
+                    <Input
+                      size="md"
+                      placeholder="ex. chromebook"
+                      className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                      {...register("name", { required: true })}
+                    />
+                  </div>
+
+                  <div className="flex justify-between mb-6">
+                    <div className="w-2/5 mb-1 flex flex-col gap-5">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="-mb-3 font-bold"
+                      >
+                        Tag Number
+                        {errors.tagNumber && (
+                          <span className="ml-3 text-xs text-red-400 italic">
+                            *This field is required
+                          </span>
+                        )}
+                      </Typography>
+                      <Input
+                        size="md"
+                        placeholder="ex. 123456789"
+                        className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                        {...register("tagNumber", { required: true })}
+                      />
+                    </div>
+                    <div className="w-2/5 mb-1 flex flex-col gap-5">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="-mb-3 font-bold"
+                      >
+                        Serial Number
+                        {errors.serialNumber && (
+                          <span className="ml-3 text-xs text-red-400 italic">
+                            *This field is required
+                          </span>
+                        )}
+                      </Typography>
+                      <Input
+                        size="md"
+                        placeholder="ex. 123456789"
+                        className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                        {...register("serialNumber", { required: true })}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="w-full mb-6 flex flex-col gap-5">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="-mb-3 font-bold"
+                    >
+                      Remarks
+                      {errors.remarks && (
+                        <span className="ml-3 text-xs text-red-400 italic">
+                          *This field is required
+                        </span>
+                      )}
+                    </Typography>
+                    <Textarea {...register("remarks")} />
+                  </div>
+
+                  <div className="w-1/3 mb-6 flex flex-col gap-5">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="-mb-3 font-bold"
+                    >
+                      Category
+                      {errors.category && (
+                        <span className="ml-3 text-xs text-red-400 italic">
+                          *This field is required
+                        </span>
+                      )}
+                    </Typography>
+                    <select
+                      className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-gray-900 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                      label="Device Type"
+                      {...register("categoryId", { required: true })}
+                    >
+                      {categories.map((category) => (
+                        <option key={category.id} value={category.id}>
+                          {category.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+
+                  <div className="flex justify-end">
+                    <Button
+                      type="submit"
+                      className="flex items-center gap-3"
+                      size="sm"
+                    >
+                      <MdAdd className="h-4 w-4" /> Add Device
+                    </Button>
+                  </div>
+                </form>
+              </CardBody>
+            </Card>
+          </div>
+        )}
+      </section>
+    </main>
+  );
+}
--- src/pages/inventory/Inventory.jsx
+++ src/pages/inventory/Inventory.jsx
@@ -1,15 +1,76 @@
+import { useEffect, useState } from "react";
 import Sidebar from "../../components/Sidebar";
 import { InventoryTable } from "./InventoryTable";
+import axios from "axios";
+import { URL } from "../../config/global";
+import { Spinner } from "@material-tailwind/react";
+import { useNavigate } from "react-router-dom";
 
 export default function Inventory() {
+  const [devices, setDevices] = useState([]); // put fetch data here
+  const [isLoading, setIsLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+
+      const response = await axios.get(`${URL}/devices`);
+
+      setDevices(response.data);
+
+      setIsLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  async function handleDelete(id) {
+    try {
+      setIsLoading(true);
+      const response = await axios.delete(`${URL}/devices/${id}`);
+      console.log(response);
+      setIsLoading(false);
+      navigate(0);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleSearch(data) {
+    try {
+      setIsLoading(true);
+
+      const response = await axios.get(`${URL}/devices?q=${data.query}`);
+
+      setDevices(response.data);
+
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <main className="bg-[#fdfdfd] h-screen">
       <Sidebar />
 
       <section className="ml-[17rem] p-8">
-        <div>
-          <InventoryTable />
-        </div>
+        {isLoading ? (
+          <div className="w-full h-screen flex justify-center items-center">
+            <Spinner />
+          </div>
+        ) : (
+          <div>
+            <InventoryTable
+              data={devices}
+              onDelete={handleDelete}
+              onNavigate={navigate}
+              onSearch={handleSearch}
+            />
+          </div>
+        )}
       </section>
     </main>
   );
